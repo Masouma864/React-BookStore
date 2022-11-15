0fc551957dec4e32e4d900b2ec0b76dc7fb03ca0
masouma <masouma2020@gmail.com>
2022-11-15 13:38:23 +0430

    configure the redux store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,11 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import addremoveReducer from './books/books';
+import categoriesReducer from './categories/categories';
+
+const rootReducer = combineReducers({
+    addremoveReducer,
+    categoriesReducer,
+  });
+  const store = configureStore({ reducer: rootReducer });
+
+export default store;
