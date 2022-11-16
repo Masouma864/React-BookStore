0852e0349091a5a858a4d80c348e8c4c1473866e
masouma <masouma2020@gmail.com>
2022-11-16 13:28:55 +0430

    Change the default state to an array with books

--- src/components/books.js
+++ src/components/books.js
@@ -4,9 +4,22 @@ import AddBook from './AddBook';
 
 const BookDisplay = () => {
   const [books] = useState([
-    { title: 'The Hunger Game', author: 'Suzanne Collins', id: 1 },
-    { title: 'Dune', author: 'Frank Herber', id: 2 },
-    { title: 'Capital in the Twenty-First Century', author: 'Suzanne Collins', id: 3 },
+    {
+       title: 'The Hunger Game',
+        author: 'Suzanne Collins',
+         id: 1 
+        },
+    {
+       title: 'Dune', 
+       author: 'Frank Herber',
+         id: 2 
+        },
+    { 
+      title: 
+      'Capital in the Twenty-First Century',
+       author: 'Suzanne Collins', 
+       id: 3 
+      },
 
   ]);
   return (
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,9 +1,28 @@
+const initialState = [
+    {
+       title: 'The Hunger Game',
+        author: 'Suzanne Collins',
+         id: 1 
+        },
+    {
+       title: 'Dune', 
+       author: 'Frank Herber',
+         id: 2 
+        },
+    { 
+      title: 
+      'Capital in the Twenty-First Century',
+       author: 'Suzanne Collins', 
+       id: 3 
+      },
+
+  ];
 // Actions
 const ADD_BOOK = 'React-BookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'React-BookStore/books/REMOVE_BOOK';
 
 // Reducer
-const addremoveReducer = (state = [], action) => {
+const addremoveReducer = (state = initialState, action) => {
   switch (action.type) {
     // do reducer stuff
     case ADD_BOOK: {
